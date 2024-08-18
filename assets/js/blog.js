f3dba5235bd375146ee821ca91a69fef578e080e
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-08-18 06:55:23 -0400

    I added styling to the blog post cards and fixed a bug that had all posts on one card

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,21 +2,21 @@
 function postBlog () {
     const post = JSON.parse(localStorage.getItem('blogPost'));
 console.log(post);
-    //post items on blogpost page
-    //create elelment which is empty p tag
-    // const blogPosts = (document.createElement(`p`));
-    //put post inside the empty string after its been stringified
-    // blogPosts.innerHTML = JSON.stringify(post)
-    //appending child to the element with id user-name
-    // document.querySelector('#user-name').appendChild(blogPosts)
+    //update to make each post its own individual card
+    //container to hold all blog post cards
+    blogContainer = document.querySelector('.blog-container');
+    
+    
+
     for (let i = 0; i < post.length; i++){   
     //creating a div element on the blog page 
     const blogPostCard = (document.createElement(`div`)) 
+    blogPostCard.classList.add('card');
     blogPostCard.innerHTML = `<h2>Blog Post</h2>
     <p><strong>Title:</strong> ${post[i].title}<span id="user-title"></span></p>
     <p><strong>Username:</strong> ${post[i].username}<span id="user-name"></span></p>
     <p><strong>Comment:</strong> ${post[i].comment}<span id="user-comment"></span></p>`
-    document.querySelector('.card').appendChild(blogPostCard)
+    blogContainer.appendChild(blogPostCard);
 }
 };
 
